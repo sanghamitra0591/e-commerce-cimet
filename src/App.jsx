5af9791c06c4d65d9a2a94195b580c8048bba61c
Joy Bose <clevercoderjoy@gmail.com>
2024-10-08 16:40:28 +0530

    cart reducer working

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import Header from "./components/Header"
-
 function App() {
 
   return (
     <>
-      <Header />
     </>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from '../slice/ProductSlice';
+import cartReducer from '../slice/CartSlice';
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
+    cart: cartReducer,
   }
 })

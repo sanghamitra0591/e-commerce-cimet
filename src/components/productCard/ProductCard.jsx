a13083d4aa282467666bbd73ab63bfa4405f5990
Joy Bose <clevercoderjoy@gmail.com>
2024-10-09 10:56:26 +0530

    product page done

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -6,10 +6,25 @@ const ProductCard = ({ id, description, title, price, image, brand }) => {
     <>
       <div className="productCard">
         <img className="productImage" src={image} alt={title} loading="lazy" />
-        <h3 className="productTitle">{title}</h3>
+        <h3 className="productTitle">{showEllipsis(title, 5)}</h3>
         <p className="productDescription">{showEllipsis(description, 10)}</p>
-        <div className="productPrice">${price}</div>
-        <div className="productBrand">{brand}</div>
+        <div className="priceBrandContainer">
+          <span className="productPrice">${price}</span>
+          <span className="productBrand">{brand}</span>
+        </div>
+        <div className="buttonsContainer">
+          <button className="addToCart">Add To Cart</button>
+        </div>
+        {/* <div className="buttonsContainer">
+          <div className="countContainer">
+            <button className="add">+</button>
+            <span className="count">0</span>
+            <button className="remove">-</button>
+          </div>
+          <div className="deleteAllContainer">
+            <button className="delete">Delete All</button>
+          </div>
+        </div> */}
       </div>
     </>
   )

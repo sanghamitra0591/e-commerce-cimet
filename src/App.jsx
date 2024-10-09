30a39921116c5992f05816e5b5af885ef1f87484
Yash777Salave <yashsalave777@rediffmail.com>
2024-10-09 11:31:38 +0530

    contact form

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,8 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="overflow-hidden">
-      {/* <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/*" element="Error" />
-        </Routes>
-        <Footer />
-      </Router> */}
-      <div>
+     
+      <div style={{paddingTop:"90px"}}>
         <Header />
         <Outlet />
         <Footer />
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,84 @@
 import React from "react";
-
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import "./Contact.css"
 const Contact = () => {
-  return <div>Contact</div>;
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required('Required'),
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Required'),
+      message: Yup.string()
+        .required('Required'),
+    }),
+    onSubmit: (values) => {
+      console.log('Form data:', values);
+      alert('Form submitted successfully!');
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit} className="contact-form">
+      <h1 className="Contact-us-Heading">Contact Us</h1>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div className="error">{formik.errors.name}</div>
+        ) : null}
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className="error">{formik.errors.email}</div>
+        ) : null}
+      </div>
+
+      <div>
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          name="message"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.message}
+        />
+        {formik.touched.message && formik.errors.message ? (
+          <div className="error">{formik.errors.message}</div>
+        ) : null}
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default Contact;
+
+
+
+
+
+

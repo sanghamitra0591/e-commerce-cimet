25d7887e43024e694752843e2f903bde423d1a0c
Sanghamitra Satpathy <sanghamitramymail@gmail.com>
2024-10-10 17:22:19 +0530

    added css changes

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -5,11 +5,13 @@ import Pagination from '../../components/pagination/Pagination';
 import ProductCard from '../../components/productCard/ProductCard';
 import "./products.css";
 import loader from "../../assets/loaders/loader.svg";
+import { setSelectedCurrency } from '../../slice/AuthSlice';
 
 const Products = () => {
   const dispatch = useDispatch();
   const products = useSelector(selectProducts);
   const loading = useSelector(selectLoading);
+  const selectedCurrency = useSelector(state=>state.selectedCurrency)
 
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -27,10 +29,28 @@ const Products = () => {
   if (loading) {
     return <div className="loading"><img src={loader} alt="loader" /></div>
   }
+  const convertPrice = (price) => {
+    if (selectedCurrency === 'USD') return price;
+    return (price * exchangeRates[selectedCurrency]).toFixed(2);
+  };
+  const changeCurrency = (currency) => {
+    setSelectedCurrency(currency);
+    products.map(el => ({
+      ...el,
+      price: convertPrice(el.price)
+    }))
+  };
 
   return (
     <>
       <h2>Products</h2>
+      <div>
+        <select onChange={(e) => changeCurrency(e.target.value)} value={selectedCurrency}>
+          <option value="USD">USD</option>
+          <option value="INR">INR</option>
+          <option value="EUR">EURO</option>
+        </select>
+      </div>
       <div className="productsContainer">
         {
           currentPageProducts.map((product) => <ProductCard key={product.id} product={product} />)
